41e3b64e256529c4f4bcf127689988eb7a438e55
mashakandler <Mashakandler@gmail.com>
2018-07-24 13:54:34 +0300

    queueu

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 var jackrabbit = require('jackrabbit');
+var {Book} = require('./models/book');
+var {Review} = require('./models/review');
+const {ObjectID} = require('mongodb');
 
 process.env.MONGODB_URI = 'mongodb://localhost:27017/bookstore';
 process.env.RABBIT_URL = "amqp://dqbsydho:psEDL1Q5l7F9j1ZbXVIY-UaoKVOxJTum@sheep.rmq.cloudamqp.com/dqbsydho";
@@ -17,7 +20,33 @@ console.log("->Start listening on Queue task_queue");
 hello.consume(onAddReview,{timeout:200});
 
 function onAddReview(data, ack) {
-  console.log("Hello, " + data.bookId + "!" + " ts:" + data.ts + " review: " + data.review);
-  ack();
-}
+  var bookId = data.bookId;
+  var review = data.review;
+  console.log("Got Review ! " + bookId+ "!" + " ts:" + data.ts + " review: " + review);
+  
+  if (!ObjectID.isValid(bookId)) {
+    console.log("objectId is not valid");
+    return 1;
+  }
+  console.log("about to find a book" + bookId);
+  Book.findById(new ObjectID(bookId),function(err,book) {
+    console.log("b1");
+    if (!book) {
+      console.log("No book with id" + id + " could be found.");
+      return 1;
+    }
+    console.log("Book Found !!!!");
+    var rev = new Review();
+    rev.bookId = bookId;
+    rev.review = review;
+    console.log("about to save review");
+    rev.save().then((doc) => {
+      console.log("Review Saved Successfully.");
+    }, (e) => {
+        console.log("Review Save Error" + e);
+    });
+  });
 
+  ack();
+  
+}
